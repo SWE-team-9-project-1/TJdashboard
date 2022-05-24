69f8c11582c59abbbbb4a2e5407c757b55683d65
Krish1903 <krishdhansinghani@gmail.com>
2022-05-24 14:03:14 -0400

    event calendar

--- src/event-calendar/EventCalendar.js
+++ src/event-calendar/EventCalendar.js
@@ -6,15 +6,4 @@ function EventCalendar(props) {
     return (<></>);
 }
 
-function App() {
-  const dateValue = new Date(new Date().getFullYear(), new Date().getMonth(), 10);
-  const minDate = new Date(new Date().getFullYear(), new Date().getMonth(), 6);
-  const maxDate = new Date(new Date().getFullYear(), new Date().getMonth(), 25);
-  return (
-    //To check calendar views paste start="Decade" and depth="Year" in below code. Also remove range restriction i.e. min and max properties
-    <CalendarComponent value={dateValue} min={minDate} max={maxDate}
-    isMultiSelection={true}></CalendarComponent>
-  );
-}
-
 export default EventCalendar;
