5bd313cf48dc9bec280c681a3853455a4795e5fd
Liam McColley <athenasbump@gmail.com>
2022-05-24 13:58:50 -0400

    class dashboard initial

--- src/class-dashboard/ClassDashboard.js
+++ src/class-dashboard/ClassDashboard.js
@@ -1,5 +1,40 @@
+import { React, useState } from "react";
+import TextField from "@mui/material/TextField";
+import ClassList from "./ClassList";
+
+
+
 function ClassDashboard(props) {
-    return (<p>Hello</p>)
+
+
+    // const [inputText, setInputText] = useState("");
+    // let inputHandler = (e) => {
+    //     //convert input text to lower case
+    //     var lowerCase = e.target.value.toLowerCase();
+    //     setInputText(lowerCase);
+    // };
+
+
+
+
+
+    return (
+        <>
+        </>
+        // <div className="main">
+        //     <h1>React Search</h1>
+        //     <div className="search">
+        //         <TextField
+        //             id="outlined-basic"
+        //             onChange={inputHandler}
+        //             variant="outlined"
+        //             fullWidth
+        //             label="Search"
+        //         />
+        //     </div>
+        //     <ClassList input={inputText} />
+        // </div>
+    );
 }
 
 export default ClassDashboard;
