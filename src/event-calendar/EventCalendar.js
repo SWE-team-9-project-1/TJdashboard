b5b3889fda8e0c2365fd2d588af23d9dcb38060b
Krish1903 <krishdhansinghani@gmail.com>
2022-05-24 15:55:11 -0400

    commit on 'eventcalendar'

--- src/event-calendar/EventCalendar.js
+++ src/event-calendar/EventCalendar.js
@@ -2,8 +2,13 @@ import React from 'react';
 import './calendar.css';
 import {CalendarComponent} from '@syncfusion/ej2-react-calendars';
 
-function EventCalendar(props) {
-    return (<></>);
-}
+function EventCalendar() {
+  var dateValue = new Date(new Date().getFullYear(), new Date().getMonth(), 24);
+  var minDate = new Date(new Date().getFullYear() - 1, new Date().getMonth(), 1);
+  var maxDate = new Date(new Date().getFullYear() + 1, new Date().getMonth(), 31);
+  return (
+    <CalendarComponent value={dateValue} min={minDate} max={maxDate}></CalendarComponent>
+  );
 
+}
 export default EventCalendar;
