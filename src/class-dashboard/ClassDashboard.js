c47ab6a492db5d86f4dbf8abef737c1303fdea58
Liam McColley <athenasbump@gmail.com>
2022-05-25 17:21:01 -0400

    Day 2 commits

--- src/class-dashboard/ClassDashboard.js
+++ src/class-dashboard/ClassDashboard.js
@@ -1,5 +1,6 @@
-import { React, useState } from "react";
-import TextField from "@mui/material/TextField";
+import { React, useState, useRef, useEffect } from "react";
+import { collection, getDocs, doc, getDoc, query, where, addDoc } from "firebase/firestore";
+import { Box, Paper, Stack, Card, TextField, Button, Autocomplete } from '@mui/material';
 import ClassList from "./ClassList";
 
 
@@ -10,30 +11,107 @@ function ClassDashboard(props) {
 
 
     const [inputText, setInputText] = useState("");
-    let inputHandler = (e) => {
+    let inputHandler = () => {
         //convert input text to lower case
-        var lowerCase = e.target.value.toLowerCase();
+        var lowerCase = searchbar.current.value.toLowerCase();
         setInputText(lowerCase);
     };
 
 
+    const [classes, setClasses] = useState([]);
+    useEffect(() => {
+        const classes = [];
+        getDocs(collection(props.db, "classes"))
+            .then((allDocs) => {
+                allDocs.forEach((doc) => classes.push(({ id: doc.id, ...doc.data() })))
+                setClasses(classes);
+            })
+    }, [props.db]);
 
 
+    const searchbar = useRef();
+    const [addClass, setAddClass] = useState(false);
+    const gradelevel = useRef();
+    const [teacherlist, setTeacherlist] = useState([]);
+    const teacherRef = useRef();
+
+
+    useEffect(() => {
+        const q = query(
+            collection(props.db, 'teachers'),
+        );
+        getDocs(q).then(dcs => setTeacherlist(dcs.docs.map(dc => {
+            return {
+
+                label: dc.data().name,
+                id: dc.id
+            };
+
+        })))
+    }, []);
+
+
+
+
+
+    const addClassToDatabase = async (name, grade) => {
+
+        let adddocfunction = await addDoc(collection(props.db, "classes"), {
+            gradeLevel: grade,
+            scores: {}
+        })
+        //need the teachers id and i think that is from the use effect right above inside the return is an id field
+        setDoc(doc(props.db, teachers, chosenID), {
+            property: value,
+        });
+
+        console.log(adddocfunction._key.path.segments[0])
+        console.log(adddocfunction._key.path.segments[1])
+
+
+
+    }
 
     return (
         <div className="main">
-            <h1>Class Dashboard</h1>
+            <Box>
+                <h1>Classes {<Button onClick={() => setAddClass(true)}>+</Button>}</h1>
+                {addClass && <Box>
+                    <Autocomplete
+                        disablePortal
+                        options={teacherlist}
+                        renderInput={(params) => <TextField {...params} label="Teacher's name" inputRef={teacherRef} />}
+                    />
+                    <p> </p>
+                    <TextField inputRef={gradelevel} label="Grade Level"></TextField>
+                    <Button
+                        onClick={() => addClassToDatabase(teacherRef.current.value, gradelevel.current.value)}
+                    >Add</Button>
+                </Box>}
+
+                {/* {addClass && <p>Teacher's Name</p>}
+                {addClass && <TextField inputRef={teacheradd}>Teacher's Name</TextField>}
+                {addClass && <p>Grade Level</p>}
+                {addClass && <TextField inputRef={gradelevel}>Grade Level</TextField>}
+                {addClass && <Button onClick={() => addClassToDatabase(teacheradd.current.value, gradelevel.current.value)}>Submit</Button>} */}
+            </Box>
+
             <div className="search">
+                <p> </p>
                 <TextField
                     id="outlined-basic"
                     onChange={inputHandler}
                     variant="outlined"
                     fullWidth
                     label="Search"
+                    inputRef={searchbar}
                 />
+
             </div>
-            <ClassList db={props.db} input={inputText} />
+            <ClassList db={props.db} input={inputText} classes={classes} />
+            <Box component="form">I AM A BOX</Box>
         </div>
+
     );
 }
 
--- src/class-dashboard/ClassList.js
+++ src/class-dashboard/ClassList.js
@@ -1,21 +1,37 @@
 import { React, useState, useEffect } from 'react'
-import data from "./ListData.json"
-import { collection, getDocs, doc, getDoc } from "firebase/firestore";
+import { collection, getDocs, doc, getDoc, query, where } from "firebase/firestore";
+import { Box, Paper, Stack, Card } from '@mui/material';
+import { Link } from 'react-router-dom';
+
 
 
 
 //figure out how to search for teachers and grade levels, possible a button that sets the state for the class component
 
-
+//This triggers a parent to rerender which triggers this to rerender, figure this shit out
 const Class = (props) => {
-    const [teacher, setTeacher] = useState([]);
-    getDoc(doc(props.db, "teachers", props.data.teacher.id)).then((doc) => setTeacher(doc.data()["name"]))
+    const [toDisplay, setToDisplay] = useState(true)
+    // const truthstatement = false;
+    // for (const [key, value] of Object.entries(props.names)) {
+    //     // if (key == props.data.id) {
+    //     //     setToDisplay(true);
+    //     // }
+    // }
+    // setToDisplay(truthstatement)
     return (
         <>
-            <tr>
-                <td>{props.data.grade}</td>
-                <td>{teacher}</td>
-            </tr>
+
+            {toDisplay && <Card>
+                <Box onClick={() => props.setSelectedClassPage(props.data.id)}>
+                    <Link to="/" className='nav-link'>
+                        {props.data.gradeLevel}{props.data.id}
+                    </Link>
+
+                </Box>
+
+            </Card>}
+
+
         </>
     )
 }
@@ -32,44 +48,75 @@ function ClassList(props) {
     //get class from teacher
     // const teach = ""
     // getDoc(doc(props.db, "students", "mE6ozwWnxCfZuyJUAqfE")).then((doc) => console.log(doc.data()["teacher"].id))
-    const teachdoc = getDoc(doc(props.db, "teachers", "SMpMCG9BawhIuN5iKvVF")).then((doc) => console.log(doc.data()["class"].id))
+    // const teachdoc = getDoc(doc(props.db, "teachers", "SMpMCG9BawhIuN5iKvVF")).then((doc) => console.log(doc.data()["class"].id))
 
     // getDoc(doc(props.db, "teachers", teach)).then((doc) => console.log(doc.data()["name"]))
 
 
+    const teacherQuery =
+        props.classes.forEach(element => query(
+            collection(props.db, 'teachers'),
+            where('class', '==', element.id)
+        ))
 
-    const [classes, setClasses] = useState([]);
+    const [displayed, setDisplayed] = useState([]);
+    const [names, setNames] = useState([]);
+    props.classes.forEach(element => console.log(element.id));
+
     useEffect(() => {
-        const classes = [];
-        getDocs(collection(props.db, "classes"))
-            .then((allDocs) => {
-                allDocs.forEach((doc) => classes.push(({ id: doc.id, ...doc.data() })))
-                setClasses(classes);
-            })
-    }, [props.db])
+        const fetchData = async () => {
+
+            const teacherQuery = query(
+                collection(props.db, 'teachers')
+            );
+            const teacherDocs = await getDocs(teacherQuery);
+
+
+            const teacherNames = {};
+            teacherDocs.forEach(element => teacherNames[element._document.data.value.mapValue.fields.class.referenceValue.slice(-20)] = element._document.data.value.mapValue.fields.name.stringValue);
 
 
 
 
 
+
+
+
+
+
+            // setDisplayed({
+            //     ...teacherDocs.docs[0].data(),
+            //     doc: teacherDocs.docs[0]
+            // });
+
+            setNames(teacherNames)
+
+        };
+        fetchData();
+    }, []);
+    const disp = {};
+    for (const [key, value] of Object.entries(names)) {
+        if (value.toLowerCase().indexOf(props.input.toLowerCase()) > -1) {
+            disp[key] = value;
+        }
+    }
+    // setDisplayed(disp)
 
     //search by teacher, grade, and student
 
-    const filteredData = data.filter((big) => {
-        if (props.input === '') {
-            return big;
-        } else {
-            return big.text.toLowerCase().includes(props.input)
-        }
-    })
+
     return (
         <>
-            <ul>
-                {filteredData.map((item) => (
-                    <li key={item.id}>{item.text}</li>
-                ))}
-            </ul>
-            {classes.map((clazz) => <Class data={clazz} db={db} />)}
+            <Stack
+                // direction='column'
+                alignItems='left'
+                spacing={1}
+            >
+                {props.classes.map((clazz) => <Class key={clazz.id} data={clazz} db={db} input={props.input} names={disp} />)}
+
+            </Stack>
+
+
         </>
     );
 }
